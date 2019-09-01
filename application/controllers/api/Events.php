abcf36c2e35a9656702155e2097574d5e384eb5b
jyothi <jyothi.m@bluenettech.com>
2019-09-01 19:08:04 +0530

    67,60,46,36,33,32,31

--- application/controllers/api/Events.php
+++ application/controllers/api/Events.php
@@ -122,9 +122,10 @@ class Events extends MY_Controller {
     public function eventNamesAjax_post() {
         $this->post = file_get_contents('php://input');
         $event_name = $this->post('eventName');
+            
 
         $userdata = $this->Main_model->userdata();
-
+        
         $query = $this->db->query("call usp_GetEventNamesForAjaxSearch('" . $event_name . "'," . $userdata->ClientId . ",@errorCode)");
         $result = $query->result_array();
 
@@ -293,5 +294,27 @@ class Events extends MY_Controller {
             $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
         }
     }
+public function getCompletedEvents_get() {
 
+        $query = $this->db->query("call usp_GetCompletedEventsDataToSendNotifications(@errorCode)");
+        $result = $query->result_array();
+
+        if ($result > 0) {
+            $output = [
+                'status' => '1',
+                'Message' => 'Data Retrived Succesfully',
+                'Row count' => count($result),
+                'Responce' => $result,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_OK); //This is the respon if success
+        } else {
+            $output = [
+                'status' => '0',
+                'Message' => 'No data found',
+                'Row count' => 0,
+                'Responce' => 0,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
 }
--- application/controllers/api/Services.php
+++ application/controllers/api/Services.php
@@ -34,6 +34,28 @@ class Services extends MY_Controller {
                 $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
             }
     }
-    
+    public function getServiceType_get() {
+
+        $query = $this->db->query("call usp_GetServiceTypes(@errorCode)");
+        $result = $query->result_array();
+
+        if ($result > 0) {
+            $output = [
+                'status' => '1',
+                'Message' => 'Data Retrived Succesfully',
+                'Row count' => count($result),
+                'Responce' => $result,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_OK); //This is the respon if success
+        } else {
+            $output = [
+                'status' => '0',
+                'Message' => 'No data found',
+                'Row count' => 0,
+                'Responce' => 0,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
 
 }
