b039e585da7699c84625374ab1c34dbc0b9e9a65
anji <anji.naga1@gmail.com>
2019-09-11 12:07:21 +0530

    send mail function

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -11,18 +11,18 @@ class Main_model extends CI_Model {
     }
 
     public function userdata() {
-        
+
         $query = $this->db->query("call usp_GetUserRoleClientDetails('" . $this->user_data->id . "',@errorCode)");
         $result = $query->row();
         return $result;
     }
-    public function upload_files($path, $title, $files)
-    { 
+
+    public function upload_files($path, $title, $files) {
 //        print_r($files);exit;
         $config = array(
-            'upload_path'   => $path,
+            'upload_path' => $path,
             'allowed_types' => 'jpg|gif|png|mp4',
-            'overwrite'     => 1,                       
+            'overwrite' => 1,
         );
 
         $this->load->library('upload', $config);
@@ -30,13 +30,13 @@ class Main_model extends CI_Model {
         $images = array();
 
         foreach ($files['name'] as $key => $image) {
-            $_FILES['images[]']['name']= $files['name'][$key];
-            $_FILES['images[]']['type']= $files['type'][$key];
-            $_FILES['images[]']['tmp_name']= $files['tmp_name'][$key];
-            $_FILES['images[]']['error']= $files['error'][$key];
-            $_FILES['images[]']['size']= $files['size'][$key];
+            $_FILES['images[]']['name'] = $files['name'][$key];
+            $_FILES['images[]']['type'] = $files['type'][$key];
+            $_FILES['images[]']['tmp_name'] = $files['tmp_name'][$key];
+            $_FILES['images[]']['error'] = $files['error'][$key];
+            $_FILES['images[]']['size'] = $files['size'][$key];
 
-            $fileName = $title .'_'. $image;
+            $fileName = $title . '_' . $image;
 
             $images[] = $fileName;
 
@@ -55,7 +55,8 @@ class Main_model extends CI_Model {
     }
 
     public function do_upload_old($userfile, $upload_path, $rename_file = 0) {
-        print_r($userfile);   exit;  
+        print_r($userfile);
+        exit;
         $config['upload_path'] = $upload_path;
         $config['allowed_types'] = 'jpg|pdf|jpeg|png';
         $config['max_size'] = 0;
@@ -68,12 +69,12 @@ class Main_model extends CI_Model {
         $this->load->library('upload', $config);
         //mutiple file upload code start
         foreach ($userfile['name'] as $key => $image) {
-            $_FILES['image_file[]']['name']= $userfile['name'][$key];
-            $_FILES['image_file[]']['type']= $userfile['type'][$key];
-            $_FILES['image_file[]']['tmp_name']= $userfile['tmp_name'][$key];
-            $_FILES['image_file[]']['error']= $userfile['error'][$key];
-            $_FILES['image_file[]']['size']= $userfile['size'][$key];
-            $fileName = 'test' .'_'. $image;
+            $_FILES['image_file[]']['name'] = $userfile['name'][$key];
+            $_FILES['image_file[]']['type'] = $userfile['type'][$key];
+            $_FILES['image_file[]']['tmp_name'] = $userfile['tmp_name'][$key];
+            $_FILES['image_file[]']['error'] = $userfile['error'][$key];
+            $_FILES['image_file[]']['size'] = $userfile['size'][$key];
+            $fileName = 'test' . '_' . $image;
             $images[] = $fileName;
 
             $this->upload->initialize($config);
@@ -81,9 +82,9 @@ class Main_model extends CI_Model {
             if ($this->upload->do_upload('images[]')) {
                 $this->upload->data();
             } else {
-                 $error = array('error' => $this->upload->display_errors());
+                $error = array('error' => $this->upload->display_errors());
 
-            print_r($error);
+                print_r($error);
                 return false;
             }
         }
@@ -107,33 +108,34 @@ class Main_model extends CI_Model {
     }
 
     //This function sends email 
-    public function send_email($email_type, $to, $data) {
-        if ($email_template = $this->getById(EMAIL_TEMPLATE, EMAIL_TEMPLATE_NAME, $email_type)) {
+    public function send_email($email_subject, $email_body, $to, $data) {
+// if ( $email_template = $this->getById( EMAIL_TEMPLATE, EMAIL_TEMPLATE_NAME, $email_type ) )
+// {
+// $msg = $this->load->view( 'forgot.php', $data, true );
+        $this->load->library('email', config_item('email_config'));
+        $this->email->set_newline("\r\n");
+        $this->email->from('jyothi.phpdeveloper@gmail.com'); // change it to yours   'dev@bluenettech.com'
+        $this->email->to($to); // change it to yours rahul.deo@talentserv.co.in
+        $this->email->subject($email_subject);
 
-            // $msg = $this->load->view( 'forgot.php', $data, true );
-            $this->load->library('email', config_item('email_config'));
-            $this->email->set_newline("\r\n");
-            $this->email->from('nap.support@bluenettech.com'); // change it to yours
-            $this->email->to($to); // change it to yours rahul.deo@talentserv.co.in
-            $this->email->subject($email_template->{EMAIL_TEMPLATE_SUBJECT});
+// $data['first_name'] = "Rahul Deo";
+// $email_body = $email_template->{EMAIL_TEMPLATE_BODY};
+// foreach ($data as $key => $value) {
+// $email_body = str_replace("{" .$key . "}", $value, $email_body);
+// }
 
-            // $data['first_name'] = "Rahul Deo";
-            $email_body = $email_template->{EMAIL_TEMPLATE_BODY};
-
-            foreach ($data as $key => $value) {
-                $email_body = str_replace("{" . $key . "}", $value, $email_body);
-            }
-
-            $this->email->message($email_body);
-            if ($this->email->send()) {
-//				echo 'Email sent.';
-                return true;
-            } else {
-                show_error($this->email->print_debugger());
-            }
+        $this->email->message($email_body);
+        if ($this->email->send()) {
+// echo 'Email sent.';
+            return true;
         } else {
-            return false;
+            show_error($this->email->print_debugger());
         }
+
+// }
+// else{
+// return false;
+// }
 
         return false;
     }
