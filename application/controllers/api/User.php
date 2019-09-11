167da296091f69781689c24dd9d2c52cb937006f
anji <anji.naga1@gmail.com>
2019-09-11 12:06:18 +0530

    user add by anji

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -8,6 +8,7 @@ class User extends MY_Controller {
         // Construct the parent class
         parent::__construct();
         $this->auth();
+        $this->load->model('Main_model');
     }
 
     public function userData_get() {
@@ -33,16 +34,17 @@ class User extends MY_Controller {
                 ];
                 $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
             }
-        }else{
+        } else {
             $output = [
-                    'status' => '0',
-                    'Message' => 'Invalid Data Provided',
-                    'Row count' => 0,
-                    'Responce' => 0,
-                ];
-                $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+                'status' => '0',
+                'Message' => 'Invalid Data Provided',
+                'Row count' => 0,
+                'Responce' => 0,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
         }
     }
+
     public function userClients_get() {
         $userId = $this->user_data->id;
         if ($userId != "") {
@@ -66,29 +68,29 @@ class User extends MY_Controller {
                 ];
                 $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
             }
-        }else{
+        } else {
             $output = [
-                    'status' => '0',
-                    'Message' => 'Invalid Data Provided',
-                    'Row count' => 0,
-                    'Responce' => 0,
-                ];
-                $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+                'status' => '0',
+                'Message' => 'Invalid Data Provided',
+                'Row count' => 0,
+                'Responce' => 0,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
         }
     }
-public function getClients_get() {
+
+    public function getClients_get() {
         $userId = $this->user_data->id;
-        $userdata=$this->Main_model->userdata();
-        $RoleId=$userdata->RoleId;
-        if($RoleId==1){
-            $clientId= "-1";
-        }
-        else{
-            $clientId=0;
+        $userdata = $this->Main_model->userdata();
+        $RoleId = $userdata->RoleId;
+        if ($RoleId == 1) {
+            $clientId = "-1";
+        } else {
+            $clientId = 0;
         }
 
         if ($userId != "") {
-            $query = $this->db->query("call usp_GetClients('".$clientId."','" . $userId . "',@errorCode)");
+            $query = $this->db->query("call usp_GetClients('" . $clientId . "','" . $userId . "',@errorCode)");
             $result = $query->result_array();
 
             if ($result > 0) {
@@ -108,31 +110,26 @@ public function getClients_get() {
                 ];
                 $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
             }
-        }else{
+        } else {
             $output = [
-                    'status' => '0',
-                    'Message' => 'Invalid Data Provided',
-                    'Row count' => 0,
-                    'Responce' => 0,
-                ];
-                $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+                'status' => '0',
+                'Message' => 'Invalid Data Provided',
+                'Row count' => 0,
+                'Responce' => 0,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
         }
     }
-    public function getUsers_get() {
+
+    public function getUsers_post() {
         $userId = $this->user_data->id;
-        $userdata=$this->Main_model->userdata();
-        $RoleId=$userdata->RoleId;
-        if($RoleId==1){
-            $clientId= "-1";
-        }
-        else{
-            $clientId=0;
-        }
+
+
+        $clientId = $this->post('clientId');
 
         if ($userId != "") {
-            $query = $this->db->query("call usp_GetUsers('".$clientId."','" . $userId . "',@errorCode)");
+            $query = $this->db->query("call usp_GetUsers('" . $userId . "','" . $clientId . "',@errorCode)");
             $result = $query->result_array();
-
             if ($result > 0) {
                 $output = [
                     'status' => '1',
@@ -150,14 +147,62 @@ public function getClients_get() {
                 ];
                 $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
             }
-        }else{
+        } else {
             $output = [
-                    'status' => '0',
-                    'Message' => 'Invalid Data Provided',
-                    'Row count' => 0,
-                    'Responce' => 0,
-                ];
-                $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+                'status' => '0',
+                'Message' => 'Invalid Data Provided',
+                'Row count' => 0,
+                'Responce' => 0,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
         }
     }
+
+    public function setUser_post() {
+        $this->post = file_get_contents('php://input');
+        if ($this->post('userId') != "") {
+            $userId = $this->post('userId');
+        } else {
+            $userId = 0;  //As per SP
+        }
+        $roleId = $this->post('roleId');
+        $clientId = $this->post('clientId');
+        $name = $this->post('name');
+        $contactNumber = $this->post('contactNumber');
+        $email = $this->post('email');
+        
+
+        $password = randomPassword();
+
+        $assignClientsIds = $this->post('assignClientsIds');
+        $disignation=$assignClientsIds !="" ? "DM Executive":"Co Ordinator";
+        $active = $this->post('active');
+        $modifiedBy = $this->user_data->id;
+        
+
+        $query = $this->db->simple_query("call usp_setUser('" . $userId . "','" . $roleId . "','" . $clientId . "','" . $name . "','" . $contactNumber . "','" . $email . "','" . $disignation . "','" . $password . "','" . $assignClientsIds . "','" . $active . "','" . $modifiedBy . "',@errorCode);");
+        
+        if ($this->db->affected_rows() > 0) {
+            
+            $subject = $disignation . ' Login Credentials';
+            $body = 'Dear User,<br /> Please find your ' . $disignation . ' login credentials below. <br /><br /> Username : ' . $name . '<br /> Password : ' . $password . '<br /><br />Thanks,<br />Ibridge Team';
+//            $mail_result=$this->Main_model->send_email( $subject, $body, $email, '' );
+            
+            $output = [
+                'status' => '1',
+                'Message' => 'Data Saved Succesfully',
+                'Row count' => $this->db->affected_rows()
+            ];
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        } else {
+            $output = [
+                'status' => '0',
+                'Message' => 'Failed to save Data',
+                'Row count' => 0,
+                'Responce' => 0,
+            ];
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
 }
