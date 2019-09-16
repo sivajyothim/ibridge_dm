0994aeca277fb5966814bc526eec254f457b8e66
jyothi <jyothi.m@bluenettech.com>
2019-09-16 23:58:11 +0530

    events issue resolved

--- application/controllers/api/Events.php
+++ application/controllers/api/Events.php
@@ -12,21 +12,44 @@ class Events extends MY_Controller {
 
     public function eventCategories_get() {
 
-        $query = $this->db->query("call usp_GetEventCategories()");
-        $result = $query->result_array();
+        
+        $canShowGenericErrorMessageToUser = false;
+        try {
+        $query = $this->db->query("call usp_GetEventCategories(@errorCode )");
+            if (!$query) {
+                $canShowGenericErrorMessageToUser = true;
+                $error   = $this->db->error();
+                throw new Exception('Query error:'.$error['code'].' '.$error['message']);
+            } else {
+                 $result = $query->result();
+                if (isset($result[0]->ErrorCode) && $result[0]->ErrorCode > 0) {
+                    if ($result[0]->ErrorCode == 45000) {
+                        // error in DB - CUSTOM MESSAGE
+                        throw new Exception(substr($result[0]->ErrorMessage, strpos($result[0]->ErrorMessage, ":") + 1));
+                    } else {
+                        // error in DB - Generic Message
+                        $canShowGenericErrorMessageToUser = true;
+                        throw new Exception($result[0]->ErrorMessage);
+                    }
+                } else {
+                    // success in DB
+                        $output = [
+                            'status' => '1',
+                            'Message' => 'Data Retrived Succesfully',
+                            'Row count' => count($result),
+                            'Responce' => $result,
+                        ];
+                        $this->set_response($output, REST_Controller::HTTP_OK); //This is the respon if success
+                     
+                }
+            }
+        } catch (Exception $e) {
 
-        if ($result > 0) {
-            $output = [
-                'status' => '1',
-                'Message' => 'Data Retrived Succesfully',
-                'Row count' => count($result),
-                'Responce' => $result,
-            ];
-            $this->set_response($output, REST_Controller::HTTP_OK); //This is the respon if success
-        } else {
+            log_message('error', 'Database:' . $e->getMessage());
+
             $output = [
                 'status' => '0',
-                'Message' => 'No data found',
+                'Message' => $canShowGenericErrorMessageToUser == true ? GENERIC_ERROR_MESSAGE : $e->getMessage(),
                 'Row count' => 0,
                 'Responce' => 0,
             ];
